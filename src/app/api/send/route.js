bb343a74a30757522011484295c1b3ac26303fc0
Md. Ashraful Islam <imdashraful17@gmail.com>
2024-01-03 17:50:15 +0600

    Bugs fixed

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+/*const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
@@ -26,3 +26,4 @@ export async function POST(req, res) {
     return NextResponse.json({ error });
   }
 }
+*/
